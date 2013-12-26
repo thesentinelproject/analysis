d56ae3cd90420a4463c1ddc98c89f7bdcd14ef9f
paradisepilot <paradisepilot@gmail.com>
2013-12-25 21:00:52 -0500

    updated: RUN_test.sh

--- dev/use-cases/Hatebase-json-to-data-frame/execution/RUN_test.sh
+++ dev/use-cases/Hatebase-json-to-data-frame/execution/RUN_test.sh
@@ -1,6 +1,6 @@
 
  myRscript=../../../code/R/test_Hatebase-json-to-data-frame.R
-  data_dir=../../downloadHatebase/output.SNAPSHOT.2013-12-24.03
+  data_dir=../../downloadHatebase/output.SNAPSHOT.2013-12-25.01
   code_dir=../../../code/R
 output_dir=../output
    tmp_dir=${output_dir}/tmp
